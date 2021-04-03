f973061d7a91511e5f32f25c14e0c541a4d4f90e
sᴡᴏɴɪᴛ <swatv3nub@gmail.com>
2021-04-03 19:49:09 +0530

    Update __init__.py

--- FilterBot/__init__.py
+++ FilterBot/__init__.py
@@ -37,35 +37,3 @@ start_uptime = time.time()
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
-
-#main app
-
-class app(Client):
-    USER: User = None
-    USER_ID: int = None
-
-    def __init__(self):
-        super().__init__(
-            "filterbot",
-            api_hash=API_HASH,
-            api_id=APP_ID,
-            plugins={
-                "root": "FilterBot/plugins"
-            },
-            workers=4,
-            bot_token=BOT_TOKEN
-        )
-        self.LOGGER = LOGGER
-
-    async def start(self):
-        await super().start()
-        usr_bot_me = await self.get_me()
-        self.set_parse_mode(markdown)
-        self.LOGGER(__name__).info(
-            f"@{usr_bot_me.username}  started! "
-        )
-        self.USER, self.USER_ID = await User().start()
-
-    async def stop(self, *args):
-        await super().stop()
-        self.LOGGER(__name__).info("Bot stopped. Bye.")
