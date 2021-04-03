a36f4e7a7d062d6d25756b141127546d1a85cd2d
sᴡᴏɴɪᴛ <swatv3nub@gmail.com>
2021-04-03 19:08:56 +0530

    Aah LUL

--- FilterBot/config.py
+++ FilterBot/config.py
@@ -1,6 +1,6 @@
 import os
 
-APP_ID = int(os.environ.get("APP_ID"))
+API_ID = int(os.environ.get("API_ID"))
 
 API_HASH = os.environ.get("API_HASH")
 
@@ -12,7 +12,7 @@ USER_SESSION = os.environ.get("USER_SESSION")
 
 # For Local Hosting, Make the above Vars as Comments and make the lower ones uncommented and correctly fill them.
 
-#APP_ID = 19288
+#API_ID = 19288
 
 #API_HASH = "my.telegram.org"
 
